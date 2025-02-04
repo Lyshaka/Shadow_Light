7434bede07469525f14f15e80123f1f046a4cc5e
Lyshaka <52411658+Lyshaka@users.noreply.github.com>
2025-02-04 21:41:27 +0100

    Increased SHADOW deceleration, changed camera value and code
    SHADOW now decelerate quicker for more control,
    Camera has bigger fov to sense the 3D aspect better, it also has a new variable to multiply the distance of the camera to have more view range

--- Assets/Project/Scripts/Gameplay/Game/CameraBehavior.cs
+++ Assets/Project/Scripts/Gameplay/Game/CameraBehavior.cs
@@ -6,7 +6,7 @@ public class CameraBehavior : MonoBehaviour
 
 	[SerializeField, Range(1f, 100f)] float cameraDistanceMin = 20f;
 	[SerializeField, Range(1f, 100f)] float cameraDistanceMax = 40f;
-	[SerializeField] float verticalOffset = 3f;
+	[SerializeField, Range(1f, 2f)] float distanceMultiplicator = 1.1f;
 	[SerializeField] float smoothDampTime = 0.3f;
 
 	float _cameraDistance = 20f;
@@ -32,12 +32,11 @@ public class CameraBehavior : MonoBehaviour
 		float verticality = Mathf.Abs(Mathf.Cos(Mathf.Atan2(direction.y, direction.x)));
 		float multiplier = Mathf.Lerp(Camera.main.aspect, 1f, verticality);
 
-		_cameraDistance = (Vector3.Distance(_lightPos, _shadowPos) * multiplier) / Mathf.Tan(Camera.main.fieldOfView * Mathf.Deg2Rad);
+		_cameraDistance = (Vector3.Distance(_lightPos, _shadowPos) * multiplier * distanceMultiplicator) / Mathf.Tan(Camera.main.fieldOfView * Mathf.Deg2Rad);
 		_cameraDistance = Mathf.Clamp(_cameraDistance, cameraDistanceMin, cameraDistanceMax);
 		
 		Vector3 targetPosition = Vector3.Lerp(_lightPos, _shadowPos, 0.5f);
 		targetPosition.z = -_cameraDistance;
-		targetPosition.y += verticalOffset;
 		
 		Camera.main.transform.position = Vector3.SmoothDamp(Camera.main.transform.position, targetPosition, ref _currentVelocity, smoothDampTime);
 	}
